636055eaae1a318df982931c7ab1b9cf9aab1458
Bruno Dias <bruno.fera09@gmail.com>
2022-01-31 07:01:46 -0300

    new

--- public/js/banner.js
+++ public/js/banner.js
@@ -2,4 +2,19 @@ let currentSlide = 0;
 let totalSlide = 2;
 
 const dots = document.querySelectorAll(".btn-banner");
-console.log(dots);
+const images = document.querySelectorAll(".banner img");
+
+function next() {
+  document
+    .querySelector(`#content${currentSlide + 1}`)
+    .classList.remove("active");
+  currentSlide = (totalSlide + currentSlide + 1) % totalSlide;
+  document.querySelector(`#content${currentSlide + 1}`).classList.add("active");
+}
+function prev() {
+  document
+    .querySelector(`#content${currentSlide + 1}`)
+    .classList.remove("active");
+  currentSlide = (totalSlide + currentSlide - 1) % totalSlide;
+  document.querySelector(`#content${currentSlide + 1}`).classList.add("active");
+}
